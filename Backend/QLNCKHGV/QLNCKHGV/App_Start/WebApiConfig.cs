6b4c4fb8578e963df11b627366812c1dc4bed01b
Chanmta <mrchanmta@gmail.com>
2019-12-19 12:12:20 +0700

    update confic error cors

--- Backend/QLNCKHGV/QLNCKHGV/App_Start/WebApiConfig.cs
+++ Backend/QLNCKHGV/QLNCKHGV/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace QLNCKHGV
 {
@@ -10,6 +11,10 @@ namespace QLNCKHGV
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            //config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(
+            //config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml"));
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -19,6 +24,9 @@ namespace QLNCKHGV
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.EnableCors(new EnableCorsAttribute("http://localhost:4200", "*", "*"));
+
         }
     }
 }
